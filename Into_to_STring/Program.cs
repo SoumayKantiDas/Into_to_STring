ce318ead4b3f4075a83aa88f5b0e2eb176cd5242
Soumay kanti Das <anikds2859@outlook.com>
2021-05-24 22:04:34 +0600

    Add project files.

---
+++ Into_to_STring/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Into_to_STring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "hellow";
+            string b = "h";
+            //Append to contests of 'b'
+            b += "ellow";
+            Console.WriteLine(a == b);
+            Console.WriteLine((object)a == (object)b);
+        }
+    }
+}
